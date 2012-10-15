d19a5be052891a5d776291a668a5766bf6f5c038
John Sposato <jsposato@gmail.com>
2012-10-15 16:44:46 -0400

    else statement added

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+	ini_set("display_errors","1");
+	
 	if(isset($_POST['submit'])) {
 		$file 		= $_POST['importFile'];
 		$user 		= htmlspecialchars($_POST['username']);
@@ -7,5 +9,7 @@
 		echo "<pre>";
 		print_r($_POST);
 		echo "</pre>";
+	} else {
+		echo "form not submitted";
 	}
 ?>
