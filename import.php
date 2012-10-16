0e5ff56ffce6e1aa6c7bfa0f4711e1e2008651ce
John Sposato <jsposato@ufl.edu>
2012-10-16 08:36:04 -0400

    Moved navigation bar to an include file

--- import.php
+++ import.php
@@ -144,26 +144,8 @@
   </head>
 
   <body>
-
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="#">FusionForge Task Importer</a>
-          <div class="nav-collapse">
-            <ul class="nav">
-              <li class="active"><a href="index.php">Home</a></li>
-              <li><a href="import.php">Import</a></li>
-              <li><a href="logout.php">Logout</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+	<!-- Include navigation bar -->
+	<?php include_once("includes/nav.php"); ?>
 
 	<!-- Main content starts here -->
     <div class="container">
